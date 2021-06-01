33fa289724475a3027395b1a73a72072658b146d
Jeremiah Schugt <jeremiah.r.schugt@gmail.com>
2021-06-01 10:41:34 -0500

    Phase 1 complete

--- Program.cs
+++ Program.cs
@@ -16,16 +16,64 @@ namespace heist
             string Answer = Console.ReadLine();
             int SkillLevel;
             bool isNum = int.TryParse(Answer, out SkillLevel);
-
-            while (!isNum)
+            try
             {
-                Console.WriteLine("Can you please enter a number");
-                Answer = Console.ReadLine();
-                isNum = int.TryParse(Answer, out SkillLevel);
+
+                while (!isNum || SkillLevel < 0)
+                {
+                    if (!isNum)
+                    {
+                        Console.WriteLine("Can you please enter a number");
+                    }
+                    else if (SkillLevel < 0)
+                    {
+                        Console.Write("Please enter a number greater than 0: ");
+                    }
+                    Answer = Console.ReadLine();
+                    isNum = int.TryParse(Answer, out SkillLevel);
+                }
+
+            }
+            catch
+            {
+                Console.WriteLine("you broke it");
+            }
+            // Validation of user input
+            // And store variables
+            // Prompt the user to enter a team member's courage factor and save that courage factor with the name.
+            Console.Write("Enter a team member's courage factor from 0.0 to 2.0: ");
+            string courageAnswer = Console.ReadLine();
+            double courageFactor;
+            isNum = double.TryParse(courageAnswer, out courageFactor);
+            try
+            {
+
+                while (!isNum || courageFactor < 0.0 || courageFactor > 2.0)
+                {
+                    if (!isNum)
+                    {
+                        Console.WriteLine("Can you please enter a number");
+                    }
+                    else if (courageFactor < 0.0 || courageFactor > 2.0)
+                    {
+                        Console.Write("Please enter a value between 0.0 and 2.0: ");
+                    }
+                    courageAnswer = Console.ReadLine();
+                    isNum = double.TryParse(courageAnswer, out courageFactor);
+                }
+
+            }
+            catch
+            {
+                Console.WriteLine("you broke it");
             }
 
-            // Prompt the user to enter a team member's courage factor and save that courage factor with the name.
+            // display team member information
+            Console.WriteLine($"Tember: {Name} Skill level: {SkillLevel} Courage Factor{courageFactor}");
 
         }
     }
 }
+
+
+
