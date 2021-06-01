3a66ed960fb75403410accf2b6f0f9c6ffb6f79d
Brandon Hill <brandonjon26@gmail.com>
2021-06-01 13:34:44 -0500

    phase 3 complete

--- Squad.cs
+++ Squad.cs
@@ -17,13 +17,28 @@ namespace heist
         {
             //? Display a message containing the number of members of the team.
             Console.WriteLine($"Member Count: {Team.Count}");
-            int i = 1;
-            foreach (TeamMember member in Team)
+            // Stop displaying each team member's information.
+            // int i = 1;
+            // foreach (TeamMember member in Team)
             //? Display each team member's information.
             {
-                Console.WriteLine($"{i++}. {member.Name}, {member.SkillLevel}, {member.CourageFactor}");
+                // Console.WriteLine($"{i++}. {member.Name}, {member.SkillLevel}, {member.CourageFactor}");
             }
 
+        }
+
+        // Sum the skill levels of the team. Save that number.
+        public int TeamLevel
+        {
+            get
+            {
+                int TeamSkillLevel = 0;
+                foreach (TeamMember SkillLevel in Team)
+                {
+                    TeamSkillLevel += SkillLevel.SkillLevel;
+                }
+                return TeamSkillLevel;
+            }
         }
     }
 
